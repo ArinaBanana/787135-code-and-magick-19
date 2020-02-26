1548e31489a47607b6f3e775a4c95c335ee68217
Arina <hadson.rey@mail.ru>
2020-02-26 14:29:44 +0300

    рефакторинг

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var characters = document.querySelector('.setup');
-var similarCharacters = document.querySelector('.setup-similar');
-var similarList = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var characters = document.querySelector('.setup');
+var similarCharacters = document.querySelector('.setup-similar');
+var similarList = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var getRandomNumber = function (a, b) {
   return Math.floor(Math.random() * (b - a) + a);
